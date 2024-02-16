e7f8dfe5c15e614c74406374695d340924260cc8
yogesh313r <149294443+yogesh313r@users.noreply.github.com>
2024-02-16 10:14:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,23 +2,20 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
-#BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-TOKEN_ONE = os.environ.get("TOKEN_ONE", "")
+API_ID = os.environ.get("API_ID", "25393663")
+API_HASH = os.environ.get("API_HASH", "46fb840e6cb4b84d582c44ebbf703251")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6475149749:AAEQt9M91pjiNCBF_lkxgVEXdq77S1lT_n0") 
 
-CHANNEL = os.environ.get("CHANNEL", "ATL_Univers") # username without '@'
-BOT_USERNAME = os.environ.get("BOT_USERNAME","") # username without '@'
+CHANNEL = os.environ.get("CHANNEL", "-1002044312409") # username without '@'
+BOT_USERNAME = os.environ.get("BOT_USERNAME","Renamebott_bot") # username without '@'
 SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP","BYNF_TamilChat") # username without '@'
 UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","ATL_Univers") # username without '@'
-OWNER_USERNAME = os.environ.get("OWNER_USERNAME","HMF_Owner_1")
-STRING = os.environ.get("STRING", "")
+OWNER_USERNAME = os.environ.get("OWNER_USERNAME","Yogi_YogiBot")
 
 DB_NAME = os.environ.get("DB_NAME","renameone")     
-DB_URL = os.environ.get("DB_URL","")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://karthickjk:karthick@cluster0.vcjskkq.mongodb.net/?retryWrites=true&w=majority")
 
 FLOOD = int(os.environ.get("FLOOD", "90"))
-LAZY_PIC = os.environ.get("LAZY_PIC", "")
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1989750989').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5324568283').split()]
 PORT = os.environ.get('PORT', '8080')
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001230390281"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002121321120"))
